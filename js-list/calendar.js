20cbbe540e52ffdad9ec01dc5a81bad7cdf31e8a
binc0 <42704408+binc0@users.noreply.github.com>
2020-03-25 01:34:15 +0100

    upload

---
+++ js-list/calendar.js
@@ -0,0 +1,26 @@
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "/data/italia.json",
+  "method": "GET",
+}
+
+
+$.ajax(settings).done(function (jcontent){
+    
+    console.log(jcontent);
+    console.log(jcontent.regioni.length);
+    console.log(jcontent.regioni);
+ document.getElementById("remove").remove();
+ 
+for (var i = 0; i < jcontent.regioni.length; i++) {
+    console.log(i);
+    console.log(jcontent.regioni[i].nome);
+
+var output = document.getElementById('regioni');
+output.innerHTML += '<div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xs-12"> <div class="sched d-block d-lg-flex"> <div class="text order-1"> <h3>' + jcontent.regioni[i].nome + '</h3> <p class="text-primary h3"> <a class="" href="' + jcontent.regioni[i].link + '" >Cerca</a> </p> </div> </div> </div>';
+
+}
+
+});
+
