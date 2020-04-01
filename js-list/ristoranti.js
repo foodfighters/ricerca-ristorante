c21da6f62bbdbc20e3172752948d000886226be6
binc0 <42704408+binc0@users.noreply.github.com>
2020-04-01 23:53:04 +0200

    ok

--- js-list/ristoranti.js
+++ js-list/ristoranti.js
@@ -29,7 +29,7 @@ function loadcomuni(cm)
      $.ajax(settings).done(function (jcontent){
     
    // console.log(jcontent);
-    console.log(jcontent.ristoranti[45].nome);
+    console.log(jcontent.ristoranti[1].nome);
     console.log(jcontent.ristoranti.length);
       
          $("#ristoranti").empty();
@@ -42,9 +42,9 @@ for (var i = 0; i < jcontent.ristoranti.length; i++) {
         for (var x = 0; x < jcontent.ristoranti[i].comune.length; x++) {
         comunix += jcontent.ristoranti[i].comune[x] + ', '; }
     
-        var divristorante = '<div class="col-xl-6 col-lg-4 col-md-6 col-sm-6 col-xs-12" id="' + jcontent.ristoranti[i].tag + '"> <div class="sched-r d-block d-lg-flex"> <a class="text-white" href="#popup-' + jcontent.ristoranti[i].code + '" onclick="popuptitle(text);"><div <div class="text order-2"> <h3 class="text-white">' +  jcontent.ristoranti[i].nome + ' </h3> <p  class="text-white" >' + jcontent.ristoranti[i].text + '</p> <p class="text-white h3">  </p> </div> </a> </div> </div>';
+        var divristorante = '<div class="col-xl-6 col-lg-4 col-md-6 col-sm-6 col-xs-12" id="' + jcontent.ristoranti[i].tag + '"> <a class="text-white" href="#popup-' + jcontent.ristoranti[i].code + '" onclick="popuptitle(text);"> <div class="sched-r d-block d-lg-flex"> <div <div class="text order-2"> <h3 class="text-white">' +  jcontent.ristoranti[i].nome + ' </h3> <p  class="text-white" >' + jcontent.ristoranti[i].text + '</p> <p class="text-white h3">  </p> </div>  </div> </a> </div>';
              
-        var divpopup = ' <div class="overlay" id="popup-' + jcontent.ristoranti[i].code + '"> <div class="sched popup d-block d-lg-flex"><div class="container" style="margin-top: 10%; margin-bottom: 10%; "> <div class="close"><a href="#close" onclick="popupclose();"><i class="fa fa-times"></i></a></div> <h4 class="text-primary">#' + jcontent.ristoranti[i].code + '</h4> <h2>' + jcontent.ristoranti[i].nome + '</h2> <p>' + jcontent.ristoranti[i].text + '</p> <p class="text-primary h4"><a href="' + jcontent.ristoranti[i].link_ordinazione + '">Link Ordinazione</a></p> <p class="text-primary h4"><a href="' + jcontent.ristoranti[i].link_facebook + '">Link Facebook <i class="fa fa-facebook"></i> </a></p> <h3>Telefono :&nbsp; </h3> <p>' + jcontent.ristoranti[i].numero + '</p> <h3>Mail :&nbsp;</h3> <a href="mailto:' + jcontent.ristoranti[i].email + '">' + jcontent.ristoranti[i].email + '</a> <h3>Consegna a:</h3> <p class="text-primary h4">' +  comunix  + '</p> <!-- Go to www.addthis.com/dashboard to customize your tools --> <div class="addthis_inline_share_toolbox"></div> </div> </div> </div> ';  
+        var divpopup = ' <div class="overlay" id="popup-' + jcontent.ristoranti[i].code + '"> <div class="sched popup d-block d-lg-flex"><div class="container" style="margin-top: 10%; margin-bottom: 10%; "> <div class="close"><a href="#close" onclick="popupclose();"><i class="fa fa-times"></i></a></div> <h4 class="text-primary">#' + jcontent.ristoranti[i].code + '</h4> <h2>' + jcontent.ristoranti[i].nome + '</h2> <p>' + jcontent.ristoranti[i].text + '</p> <p class="text-primary h4"><a href="' + jcontent.ristoranti[i].link_ordinazione + '">Link Ordinazione</a></p> <p class="text-primary h4"><a href="' + jcontent.ristoranti[i].link_facebook + '">Link Facebook <i class="fa fa-facebook"></i> </a></p> <h3>Telefono :&nbsp; </h3> <p>' + jcontent.ristoranti[i].numero + '</p> <h3>Mail :&nbsp;</h3> <a href="mailto:' + jcontent.ristoranti[i].email + '">' + jcontent.ristoranti[i].email + '</a> <h3>Consegna a:</h3> <p class="text-primary h4">' +  comunix  + '</p> <div class="addthis_inline_share_toolbox"></div> </div> </div> </div> ';  
 
         
         var output = document.getElementById('ristoranti');
