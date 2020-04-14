1744d041bfb0240da81b77d0969a8eab8fbdf79a
binc0 <42704408+binc0@users.noreply.github.com>
2020-04-14 23:36:22 +0200

    contributors

--- js-list/contributors.js
+++ js-list/contributors.js
@@ -1,4 +1,5 @@
 var settings = {
+    "dataType": "json",
   "async": true,
   "crossDomain": true,
   "url": "https://raw.githubusercontent.com/foodmule/contributors/master/.all-contributorsrc",
@@ -12,12 +13,12 @@ $.ajax(settings).done(function (jcontent){
   console.log(jcontent.contributors);
  document.getElementById("remove").remove();
  
-for (var i = 0; i < 1; i++) {
+for (var i = 0; i < jcontent.contributors.length; i++) {
     console.log(i);
     console.log(jcontent.contributors[i].name);
 
 var output = document.getElementById('contributors');
-output.innerHTML += '<div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12"> <div class="sched-r d-block d-lg-flex"> <a class="text-white" href="' + jcontent.contributors[i].profile + '" > <div class="text order-1"> <h4 class="text-white">' + jcontent.contributors[i].name + '</h4> <p class="text-white h3">Cerca</p> </div> </a> </div> </div> </div>';
+output.innerHTML += '<div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-12"> <div class="sched-r d-block d-lg-flex text-center"> <a class="text-white" href="' + jcontent.contributors[i].profile + '" > <div class="text order-1"> <img class="rounded-img" height="100px" src="' + jcontent.contributors[i].avatar_url + '"></img> <h4 class="text-white">' + jcontent.contributors[i].name + '</h4> <p class="text-white h3">Ha contribuito a : ' + jcontent.contributors[i].contributions + '</p> </div> </a> </div> </div> </div>';
 
 }
 
